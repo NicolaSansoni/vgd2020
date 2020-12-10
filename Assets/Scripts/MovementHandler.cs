2a879ba22e1cb4bdcae5f9b56c951e1e85833054
Nicola Sansoni <nsansoni97@gmail.com>
2020-12-10 15:38:52 +0100

    Improve movement

--- Assets/Scripts/MovementHandler.cs
+++ Assets/Scripts/MovementHandler.cs
@@ -10,11 +10,11 @@ public class MovementHandler : MonoBehaviour
     public float maxSpeed = 6f;
     public float acceleration = 20f;
     public float rotXSec = 2f;
-    public float maxSlope = 50f;
 
     private Rigidbody rb;
-    private Collider feet;
     private ContactPoint[] contacts;
+    private Vector3 wallNormal;
+    private Vector3 groundNormal;
     private Vector3 movInput;
     private bool jmpInput;
     private Vector3 planeVel;
@@ -24,26 +24,31 @@ public class MovementHandler : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         contacts = new ContactPoint[10];
-        for (int i = 0; i < contacts.Length; i++) {
+        for (int i = 0; i < contacts.Length; i++)
+        {
             contacts[i] = new ContactPoint();
         }
+        wallNormal = new Vector3();
+        groundNormal = -Physics.gravity;
         movInput = new Vector3();
         jmpInput = false;
         planeVel = Vector3.zero;
-        feet = transform.Find("Base").gameObject.GetComponent<Collider>();
         isGrounded = false;
     }
 
-    private void Update() {
+    private void Update()
+    {
         movInput.x = input.getMovement().x;
         movInput.z = input.getMovement().y;
 
-        if (input.getJump()) {
+        if (input.getJump())
+        {
             jmpInput = true;
         }
 
         // keep char facing forward when moving, smooth the rotation
-        if (planeVel.magnitude > 0.1f) {
+        if (planeVel.magnitude > 0.1f)
+        {
             Quaternion fwdRot = Quaternion.LookRotation(planeVel, -Physics.gravity);
             fwdRot = Quaternion.RotateTowards(transform.rotation, fwdRot, 360f * rotXSec * Time.deltaTime);
             transform.rotation = fwdRot;
@@ -52,68 +57,105 @@ public class MovementHandler : MonoBehaviour
 
     void FixedUpdate()
     {
-        planeVel = Vector3.ProjectOnPlane(rb.velocity, Physics.gravity);
-        Vector3 velTarget = movInput * maxSpeed;
+        planeVel = Vector3.ProjectOnPlane(rb.velocity, -Physics.gravity);
+        Vector3 groundVel = Vector3.ProjectOnPlane(rb.velocity, groundNormal);
         // align with the camera
+        Vector3 movTarget = movInput;
         Quaternion camUpRot = Quaternion.FromToRotation(pointOfView.up, Vector3.up);
         Vector3 camFwd = camUpRot * pointOfView.forward;
         Quaternion fwdRot = Quaternion.FromToRotation(Vector3.forward, camFwd);
-        Quaternion upRot = Quaternion.FromToRotation(Vector3.up, -Physics.gravity);
-        velTarget = upRot * fwdRot * velTarget;
-        
-        Vector3 velDiff = velTarget - planeVel;
+        Quaternion upRot = Quaternion.FromToRotation(Vector3.up, groundNormal);
+        movTarget = upRot * fwdRot * movTarget;
+        // adjust for slopes
+        float verticalComponent = Vector3.Dot(movTarget, -Physics.gravity.normalized);
+        Debug.Log(verticalComponent);
+        movTarget -= movTarget * verticalComponent;
+        // difference between desired actual and velocity
+        Vector3 velDiff = movTarget * maxSpeed - groundVel;
         // clamp instead of normalize to handle standing still correctly
         Vector3 accelVect = Vector3.ClampMagnitude(velDiff, 1f) * acceleration;
+        // Stop sticking to walls
+        wallNormal.Normalize();
+        Debug.DrawRay(transform.position, groundNormal, Color.red);
+        float wallComponent = Vector3.Dot(accelVect, wallNormal);
+        if (wallComponent < 0)
+        {
+            accelVect -= wallComponent * wallNormal;
+        }
+        wallNormal = Vector3.zero;
+        // apply
         rb.AddForce(accelVect, ForceMode.Acceleration);
 
-        if (jmpInput) {
-            if (getGrounded()){
+        if (jmpInput)
+        {
+            if (getGrounded())
+            {
                 Jump();
             }
             jmpInput = false;
         }
     }
 
-    private void Jump() {
+    private void Jump()
+    {
         float jumpSpeed = Mathf.Sqrt(jumpHeight * 2f * Physics.gravity.magnitude);
         rb.velocity = planeVel + -Physics.gravity.normalized * jumpSpeed;
     }
 
-    public float getSpeed() {
+    public float getSpeed()
+    {
         return planeVel.magnitude;
     }
-    public bool getJump() {
+    public bool getJump()
+    {
         return jmpInput && getGrounded();
     }
-    public bool getGrounded() {
+    public bool getGrounded()
+    {
         return isGrounded;
     }
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnCollisionEnter(Collision other)
+    {
         int nCollisions = other.GetContacts(contacts);
-        for (int i = 0; i < nCollisions; i++) {
-            if (contacts[i].thisCollider.Equals(feet) &&
-                !contacts[i].otherCollider.CompareTag("Character") &&
-                Vector3.Angle(contacts[i].normal, -Physics.gravity) < maxSlope) {
-
-                Debug.Log(contacts[i].otherCollider);
+        for (int i = 0; i < nCollisions; i++)
+        {
+            ContactPoint cp = contacts[i];
+            /* Ground check */
+            if (Vector3.Angle(cp.normal, -Physics.gravity) < 90f)
+            {
                 isGrounded = true;
+                groundNormal = cp.normal;
+            }
+            /* Stop sticking to walls */
+            else
+            {
+                wallNormal += cp.normal;
             }
         }
     }
-    private void OnCollisionStay(Collision other) {
+    private void OnCollisionStay(Collision other)
+    {
         int nCollisions = other.GetContacts(contacts);
-        for (int i = 0; i < nCollisions; i++) {
-            if (contacts[i].thisCollider.Equals(feet) &&
-                !(contacts[i].otherCollider.CompareTag("Character")) &&
-                Vector3.Angle(contacts[i].normal, -Physics.gravity) < maxSlope) {
-
-                Debug.Log(contacts[i].otherCollider);
+        for (int i = 0; i < nCollisions; i++)
+        {
+            ContactPoint cp = contacts[i];
+            /* Ground check */
+            if (Vector3.Angle(cp.normal, -Physics.gravity) < 90f)
+            {
                 isGrounded = true;
+                groundNormal = cp.normal;
+            }
+            /* Stop sticking to walls */
+            else
+            {
+                wallNormal += cp.normal;
             }
         }
     }
-    private void OnCollisionExit(Collision other) {
+    private void OnCollisionExit(Collision other)
+    {
         isGrounded = false;
+        groundNormal = -Physics.gravity;
     }
 }
